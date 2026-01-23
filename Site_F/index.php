b4af59f11366730d67da90a1adc85f417dc78b44
Arthur_Chakoualeu <arthurtotie@gmail.com>
2026-01-23 12:41:28 +0100

    les interfaces de du SPRINT 1

---
+++ Site_F/index.php
@@ -0,0 +1,8 @@
+<?php 
+    include 'includes/link.php';
+    include 'includes/navbar.php' ;
+   
+    include 'public/concours.php' ;
+     include 'public/concours_detail.php' ;
+     include 'includes/footer.php' ;
+?>
