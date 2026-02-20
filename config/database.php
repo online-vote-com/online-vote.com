c91bd1a1d632f303ae8da40f8259d15d23787778
Arthur_Chakoualeu <arthurtotie@gmail.com>
2026-02-21 00:42:29 +0100

    ajout du système d'autentification basique

--- config/database.php
+++ config/database.php
@@ -1,5 +1,5 @@
 <?php 
-   $dsn = "mysql:host=127.0.0.1;port=3306;dbname=online-vote";
+  /* $dsn = "mysql:host=127.0.0.1;port=3306;dbname=online-vote";
 
    try{
       $pdo = new PDO($dsn, "root", "");
@@ -8,5 +8,17 @@
       echo "Erreur lors de la connexion : " . $e->getMessage();
       $pdo =Null;
    }
+      */
 
+
+   $dsn = "mysql:host=localhost;port=3306;dbname=u636319906_online_vote";
+
+   try{
+      $pdo = new PDO($dsn, "u636319906_admin", "So_lo@.12");
+   // echo "<h1>Succès lors de la connexion</h1>";
+   }catch(PDOException $e){
+      echo "Erreur lors de la connexion : " . $e->getMessage();
+      $pdo =Null;
+   }
+  
 ?>
