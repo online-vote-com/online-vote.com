50b841665d9153fe30008293dd2084bbdb90cd17
Arthur_Chakoualeu <arthurtotie@gmail.com>
2026-03-12 16:04:33 +0100

    authentification itération 2

--- login.php
+++ login.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 session_start();
-include 'includes/link.php'; 
+//include 'includes/link.php'; 
 
 ?>
 <link rel="stylesheet" href="assets/css/register.css">
