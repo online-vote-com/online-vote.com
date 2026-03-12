3b5f20e217f37d91e342c8e125f356471d091d86
Arthur_Chakoualeu <arthurtotie@gmail.com>
2026-03-12 16:18:15 +0100

    authentification itération 2

---
+++ login1.php
@@ -0,0 +1,37 @@
+
+<?php 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+
+?>
+<link rel="stylesheet" href="assets/css/register.css">
+<link rel="stylesheet" href="assets/css/color.css">
+<div class="main-container">
+    <div class="sidebar">
+        <h1>Donne une nouvelle tournure à ton concours, compétition</h1>
+    </div>
+        <div class="alert">
+            </div>
+           <div class="form-wrapper">
+    <h2>Connexion</h2>
+    <hr class="divider">
+    <form class="form-section" method="post" action="auth/login.php">
+        <div class="input-group">
+            <label>Email :</label>
+            <input type="email" name="email" required>
+        </div>
+        <div class="input-group">
+            <label>Mot de passe :</label>
+            <input type="password" name="mdp" required>
+        </div>
+        <button type="submit" class="btn-submit">Se connecter</button>
+    </form>
+</div>
+                
+
+            <p class="footer-text">Pas de compte ? <a href="register">inscription</a></p>
+            <p class="footer-text">Retour à <a href="index">l'acceuil</a></p>
+        </div>
+    </div>
+</div>
+
