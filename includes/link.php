a1dec2aec2733848164b4a7d9e3e0b5f039e9cfe
Arthur_Chakoualeu <arthurtotie@gmail.com>
2026-05-20 20:49:51 +0100

    modifcation mme tamo relook login et register hero section logo navbar à jour

--- includes/link.php
+++ includes/link.php
@@ -15,12 +15,11 @@
     <!-- CSS -->
     <link rel="stylesheet" href="<?= BASE_URL ?>assets/css/color.css">
     <link rel="stylesheet" href="<?= BASE_URL ?>assets/css/tableau.css">
-    <link rel="stylesheet" href="<?= BASE_URL ?>assets/css/footer.css">
     <link rel="stylesheet" href="<?= BASE_URL ?>assets/css/sidebar.css">
     <link rel="stylesheet" href="<?= BASE_URL ?>assets/css/newsidebar.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>assets/css/profile_candidat.css">
    <link rel="stylesheet" href="<?= BASE_URL ?>assets/css/concours_detail.css">
     <link rel="stylesheet" href="<?= BASE_URL ?>assets/css/formulaire_nouveau.css">
-    <link rel="stylesheet" href="<?= BASE_URL ?>assets/css/navbar.css">
     <link rel="stylesheet" href="<?= BASE_URL ?>assets/css/admin-detailconcours.css">
 
     <!-- JS -->
